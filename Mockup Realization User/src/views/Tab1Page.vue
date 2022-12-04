ab5c9167222d6e5a5db0ecc5382b3fd12f03cf02
YohanesKukuh20 <114045039+YohanesKukuh20@users.noreply.github.com>
2022-12-05 00:51:47 +0700

    add mockup realization user

---
+++ Mockup Realization User/src/views/Tab1Page.vue
@@ -0,0 +1,87 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title><center>Register User</center></ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <h1><center>Hello, Welcome!</center></h1>
+      <ion-item class="input" fill="solid">
+        <ion-input placeholder="NickName" class="custom"></ion-input>
+      </ion-item>
+    
+      <ion-item class="input" fill="solid">
+        <ion-input placeholder="PhoneNumber" class="custom"></ion-input>
+      </ion-item>
+      
+      <ion-item class="input" fill="outline">
+        <ion-input placeholder="Password" class="custom"></ion-input>
+        
+      </ion-item>
+      
+      <ion-item class="input" fill="outline">
+        <ion-input placeholder="Name" class="custom"></ion-input>
+        
+      </ion-item>
+
+      <center><button class="button" type="button">Save Changes</button></center>
+      
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large"></ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <ExploreContainer name="" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<style scoped>
+ion-title{
+  background-color: orange;
+  color: black;
+  height: 50px;
+
+}
+
+ion-input.custom {
+  --background: #FFD384;
+  --color: black;
+  --placeholder-color: black;
+  --placeholder-opacity: .8;
+
+  --padding-bottom: 10px;
+  --padding-end: 10px;
+  --padding-start: 10px;
+  --padding-top: 10px;
+  width: 25px;
+}
+
+.button{
+    background-color: orange;
+    border: none;
+    color: black;
+    padding: 10px 30px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+
+  }
+  ion-item{
+      align-items: center;
+    }
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default  defineComponent({
+  name: 'Tab1Page',
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+});
+</script>
---
+++ Mockup Realization User/src/views/Tab3Page.vue
@@ -0,0 +1,98 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title><center>User</center></ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+     
+      <ion-item fill="solid">
+        <ion-input id="input" placeholder="First Name" class="custom"></ion-input>
+      </ion-item>
+    
+      <ion-item fill="solid">
+        <ion-input placeholder="Last Name" class="custom"></ion-input>
+      </ion-item>
+      
+      <ion-item fill="outline">
+        <ion-input placeholder="Phone Number" class="custom"></ion-input>
+        
+      </ion-item>
+      
+      <ion-item fill="outline">
+       
+      </ion-item>
+
+      <center><button class="button" type="button">Save Changes</button></center>
+      
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large"></ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <ExploreContainer name="" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<style scoped>
+ion-title{
+  background-color: orange;
+  color: black;
+  height: 50px;
+
+}
+ion-input.custom {
+  --background: #FFD384;
+  --color: black;
+  --placeholder-color: black;
+  --placeholder-opacity: .8;
+
+  --border-radius: 5px;
+  --padding-bottom: 10px;
+  --padding-end: 10px;
+  --padding-start: 10px;
+  --padding-top: 10px;
+}
+
+.button{
+    background-color: orange;
+    border: none;
+    color: black;
+    padding: 10px 30px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+
+  }
+
+  .custom{
+  --background: #ff884b;
+  --color: #000;
+  --border-radius: 10px;
+  --border-color: white;
+  --border-style: solid;
+  --border-width: 1px;
+  --padding-top: 10px;
+  --padding-bottom: 10px;
+  margin-right: 10px;
+
+}
+  ion-item{
+      align-items: center;
+    }
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default  defineComponent({
+  name: 'Tab1Page',
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+});
+</script>
