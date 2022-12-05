e68f1726752100fa2e1c843924a646cd343a4652
Muhamad Tegar Fitriano Jagat <89504891+mtegarfitrianoj28@users.noreply.github.com>
2022-12-05 23:08:31 +0700

    Update channel.py

--- mahameru/channel.py
+++ mahameru/channel.py
@@ -1,85 +1,88 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask_pymongo import PyMongo
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from flask import jsonify,request
+from werkzeug.security import generate_password_hash,check_password_hash
 import uuid
-from flask import Blueprint, render_template, abort
-from jinja2 import TemplateNotFound
-from editchannel import *
 
-bp = Blueprint('channel', __name__,
-                        template_folder='templates')
-
-
-# placeholder data
-'''user = {
-    'userid': '',
+'''Channel = {
+    '_id' : '',
+    'owner_id' : '',
+    'members_id' : '',
     'name': '',
-    'no_telp': '',
-    'pin': '',
+    'description: '',
     'created_at': '',
     'updated_at': '',
-    'contact_id': ''
 }'''
 
-# User 
-@bp.route('/get', methods=['GET'])
-def getchannel():
-    data = getchannel()
-    return data
+app = Flask(__name__)
+app.secret_key = "secretkey"
 
-# TODO : fix this so it match with the given docs
+app.config['MONGO_URI'] = "mongodb://localhost:27017/mahamerudb"
+mongo = PyMongo(app)
 
-@bp.route('/createchannel', methods=['POST'])
-def createchannel():
-        try:
-            # for testing purpose only getting one request json
-            bp['name'] = request.json['channel']
-            bp['created_at'] = request.json['created_at']
-            bp['updated_at'] = request.json['updated_at']
-            bp['userid'] = uuid.uuid4()
+@app.route('/add',methods=['POST']) # KELAR
+def add_channel():
+    _json = request.json
+    _name = _json['name']
+    _desc = _json['description']
+    _owner = _json['owner_id']
+    _members = _json['members_id']
 
-            response = jsonify({'id' : bp['channel.id']})
-            response.status_code = 200
-        except Exception as e:
-            print(e)
-            response = jsonify({'message' : 'Failed to create channel'})
-            response.status_code = 400
-        finally:
-            return response
+    if _name and _desc and _owner and _members and request.method == "POST":
+        _id = mongo.db.channel.insert_one({"name": _name,"desc": _desc, "owner_id" : _owner,  "members_id" : _members })
+        resp = jsonify("channel Added successfully")
+        resp.status_code = 200
+        return resp
+    else:
+        return not_found()
 
-# TODO : dont modify it untill the above route is fixed
-@bp.route('/createchannel', methods=['PUT'])
-def edituser():
-        try:
-            bp['channel.id'] = request.json['channel.id']
-            bp['name'] = request.json['name']
-            bp['updated_at'] = request.json['updated_at']
+@app.route('/update/<id>',methods=['PUT']) # jadiin update channel
+def update_channel(id):
+    _id = id
+    _json = request.json
+    _name = _json['name']
+    _desc = _json['description']
+    _owner = _json['owner_id']
+    _members = _json['members_id']
 
-            response = jsonify({'id' : bp['channel.id']})
-            response.status_code = 200
-        except Exception as e:
-            print(e)
-            response = jsonify({'message': 'Failed to create channel'})
-            response.status_code = 400
-        finally:
-            return response
+    if _name and _desc and _owner and _members and request.method == "PUT":
+        mongo.db.channel.update_one({'_id':ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)},{'$set':{'name':_name,'description':_desc,'owner_id:': _owner, "members_id" : _members}})
+        resp = jsonify("User updated successfully")
+        resp.status_code = 200
+        return resp
+    else:
+        return not_found()
 
-@bp.route('/createchannel/<string:channel_id>', methods=['GET'])
-def newchannel(channel_id):
-    try:
-        # this code must be changed after implementing the database
-        if channel_id == '2c535c8b-5d2b-4a72-9268-1c83aaf61902':
-            response = jsonify({'Channel': 'Group 1'})
-            response.status_code = 200
-    except Exception as e:
-        print(e)
-        response = jsonify({'message' : 'Failed to get channel'})
-        response.status_code = 400
-    finally:
-        return response
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status':404,
+        'message':'Not Found' + request.url
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
 
-@bp.route('/delchannel', methods=['DELETE'])
-def deletechannel():
-    # this code must be fixed after implmenting the database and getting clarity from director
-    return jsonify({'mesage' : 'deleting the channel that you want'})
+@app.route('/show') #tampilin channel (kelar)
+def channel_all():
+    channel = mongo.db.channel.find()
+    resp = dumps(channel)
+    return resp
 
-if __name__ == "__channel__":
-        bp.run(debug=True)
+@app.route('/channel/<id>') # tampilin channel sesuai dengan channel ID
+def channel_one(id):
+    channel = mongo.db.channel.find_one({'_id':ObjectId(id)})
+    resp = dumps(channel)
+    return resp
+
+@app.route('/delete/<id>',methods=['DELETE']) # hapus channel sesuai dengan channel ID
+def delete_channel(id):
+    _id = mongo.db.channel.delete_one({'id':ObjectId(id)})
+    resp = jsonify("channel deleted successfully")
+    resp.status_code = 200
+    return resp
+
+if __name__ == "__main__":
+    app.run(debug=True)
